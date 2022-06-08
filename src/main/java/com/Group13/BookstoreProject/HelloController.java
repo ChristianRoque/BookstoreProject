6fef4624833851153004fd6dd5e5cdca2664d5a9
ChristianRoque <croquevaldes@hotmail.com>
2022-06-08 17:28:22 -0400

    adding mongodb dependency to spring

--- src/main/java/com/Group13/BookstoreProject/HelloController.java
+++ src/main/java/com/Group13/BookstoreProject/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Hello World from template!";
     }
 }
