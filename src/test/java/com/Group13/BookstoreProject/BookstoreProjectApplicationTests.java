342d4f582d75cac84de5e599a044f4d1907fe8be
ChristianRoque <croquevaldes@hotmail.com>
2022-06-08 16:13:47 -0400

    initializing the Spring template

---
+++ src/test/java/com/Group13/BookstoreProject/BookstoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Group13.BookstoreProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
