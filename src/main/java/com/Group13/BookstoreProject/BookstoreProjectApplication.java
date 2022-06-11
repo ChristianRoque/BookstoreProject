bb77ec8f6e6532db037a8555df9053bdf1257fd8
ChristianRoque <croquevaldes@hotmail.com>
2022-06-11 01:51:36 -0400

    adding enviromental variables and refactoring code from main

--- src/main/java/com/Group13/BookstoreProject/BookstoreProjectApplication.java
+++ src/main/java/com/Group13/BookstoreProject/BookstoreProjectApplication.java
@@ -4,28 +4,38 @@ import com.Group13.BookstoreProject.models.Customer;
 import com.Group13.BookstoreProject.repositories.CustomerRepository;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import com.mongodb.client.MongoClients;
 
+
 @SpringBootApplication
-public class BookstoreProjectApplication {
+public class BookstoreProjectApplication implements CommandLineRunner {
 
 	@Autowired
 	private CustomerRepository repository;
-	private static final Log log = LogFactory.getLog(BookstoreProjectApplication.class);
-	static private String uri = "mongodb+srv://croquevaldes:i5vD1aIoTWHWCcpO@cluster0.1fow7.mongodb.net/?retryWrites=true&w=majority";
+	private static final org.apache.commons.logging.Log log = LogFactory.getLog(BookstoreProjectApplication.class);
+	public static final Logger Log = LoggerFactory.getLogger(BookstoreProjectApplication.class);
+	@Value("${mongo_uri}")
+	private String uri;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreProjectApplication.class, args);
+	}
+
+	public void run(String... arg0) throws Exception {
 
 		ConnectionString connectionString = new ConnectionString(uri);
 		MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
