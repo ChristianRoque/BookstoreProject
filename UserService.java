733523f413d210cdf58c030581105f1a0a9eb317
Harreck <31360557+Harreck@users.noreply.github.com>
2022-07-26 22:50:56 -0400

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,51 @@
+package com.Group13.BookstoreProject.services;
+
+import com.Group13.BookstoreProject.models.User;
+import com.Group13.BookstoreProject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRep;
+
+    public void createUser(User user) {
+        userRep.insert(user);
+    }
+
+    public List<User> findAllUsers() {
+        return userRep.findAll();
+    }
+
+    public User findUserByUsername(String username) {
+        return userRep.findByUsername(username);
+    }
+
+    public void updateUser(User user) {
+        User dbUser = userRep.findByUsername(user.getUsername());
+        if (dbUser != null) {
+            user.setEmailAddress(dbUser.getEmailAddress());
+            user.setId(dbUser.getId());
+            userRep.save(user);
+        }
+    }
+
+    public void deleteUser(String userName) {
+        userRep.deleteById(userRep.findByUsername(userName).getId());
+    }
+
+    public void createUserCard(String creditCardInfo,String userName) {
+        User dbUser = userRep.findByUsername(userName);
+        if (dbUser != null) {
+            dbUser.addCreditCard(creditCardInfo);
+            userRep.save(dbUser);
+        }
+    }
+
+    public List<String> getUserCreditCards(String userName) {
+        return userRep.findByUsername(userName).getCreditCards();
+    }
+}
