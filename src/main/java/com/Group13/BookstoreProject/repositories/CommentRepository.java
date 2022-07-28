71729a7a548cdc5fecb083e1edfd47a860ff257d
ARob110 <107314922+ARob110@users.noreply.github.com>
2022-07-28 17:06:06 -0400

    next to final changes
    added average function + implemented basis for sorting

--- src/main/java/com/Group13/BookstoreProject/repositories/CommentRepository.java
+++ src/main/java/com/Group13/BookstoreProject/repositories/CommentRepository.java
@@ -3,6 +3,7 @@ package com.Group13.BookstoreProject.repositories;
 import com.Group13.BookstoreProject.Comment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,4 +16,8 @@ public interface CommentRepository extends MongoRepository<Comment, String> {
     List<Comment> findUserByID(String userId);
     @Query("{date: '?0'}")
     List<Comment> findDateBy(String date);
+   //@Query("{rating: '?0}")
+    //List<Comment> findRating(double rating);
+   //@Query("Select rating FROM comments rating WHERE comments.id=:id")
+    //List<Comment> getById(@Param("id")String id);
 }
